d432a90f65ad1de63c09863de466d0b6d8d11ab5
Xkonti <xkonti@xkonti.tech>
2024-06-09 08:54:29 -0400

    End of stream

--- src/nim_to_html.nim
+++ src/nim_to_html.nim
@@ -1,5 +1,107 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import std/[strutils, sequtils, macros]
 
-when isMainModule:
-  echo("Hello, World!")
+# template withFile(f: untyped, filename: string, mode: FileMode,
+#                   body: untyped) =
+#   let fn = filename
+#   var f: File
+#   if open(f, fn, mode):
+#     try:
+#       body
+#     finally:
+#       close(f)
+#   else:
+#     quit("cannot open: " & fn)
+
+proc formatArgument(name: string, value: string): string =
+  name & "=\"" & value & "\""
+
+proc combineArgs(args: varargs[string]): string =
+  args.join(" ")
+
+template defineArgumentsProc(arg: untyped, argName: string, argType: untyped) =
+  proc arg(args: seq[string],value: argType): seq[string] =
+    return args & formatArgument(argName, $value)
+
+  proc arg(value: argType): seq[string] =
+    return @[].arg(value)
+
+defineArgumentsProc(hidden, "hidden", bool)
+
+proc class(args: seq[string], classes: varargs[string]): seq[string] =
+  return args & formatArgument("class", classes.join(" "))
+
+proc class(classes: varargs[string]): seq[string] =
+  return @[].class(classes)
+
+proc style(args: seq[string], styles: varargs[string]): seq[string] =
+  return args & formatArgument("style", styles.join(";"))
+
+proc style(styles: varargs[string]): seq[string] =
+  return @[].style(styles)
+
+macro buttonMacro(body: untyped): untyped =
+  body.expectKind nnkStmtList
+  var bodyNode: NimNode = newLit("")
+  for statement in body:
+    bodyNode = quote do:
+      `bodyNode` & `statement`
+  quote do:
+    "<button>" & `bodyNode` & "</button>"
+
+macro combineStatements(body: untyped): untyped =
+  body.expectKind nnkStmtList
+  var bodyNode: NimNode = newLit("")
+  for statement in body:
+    bodyNode = quote do:
+      `bodyNode` & `statement`
+  return bodyNode
+
+
+template defineTagWithBody(tagDef: untyped, tagName: string) =
+  macro tagDef(body: untyped): string =
+    #body.expectKind nnkStmtList
+    result = newLit("")
+    for statement in body:
+      result = newCall("&", result, statement)
+    result = quote do:
+      "<" & tagName & ">" & `result` & "</" & tagName & ">"
+
+  # macro tagDef(args: seq[string], body: untyped): untyped =
+  #   body.expectKind nnkStmtList
+  #   var bodyNode: NimNode = newLit("")
+  #   for statement in body:
+  #     bodyNode = quote do:
+  #       `bodyNode` & `statement`
+  #   quote do:
+  #     "<" & tagName & " " & args.combineArgs & ">" & `bodyNode` & "</" & tagName & ">"
+
+  # template tagDef(args: seq[string], body: untyped): string =
+  #   const bodyString = myMacro:
+  #     body
+  #   "<" & tagName & " " & args.combineArgs & ">" & bodyString & "</" & tagName & ">"
+
+defineTagWithBody(button, "button")
+defineTagWithBody(bDiv, "div")
+defineTagWithBody(span, "span")
+defineTagWithBody(p, "p")
+defineTagWithBody(article, "article")
+defineTagWithBody(main, "main")
+defineTagWithBody(a, "a")
+defineTagWithBody(body, "body")
+defineTagWithBody(head, "head")
+defineTagWithBody(html, "html")
+defineTagWithBody(nav, "nav")
+
+
+const htmlString = html:
+  body:
+    main:
+      button:
+        "Home"
+      button:
+        "About"
+    article:
+      p:
+        "Hello There!"
+
+echo htmlString
